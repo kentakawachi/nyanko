2c91314f20ba9380d49d8d91ae514c45045eafbd
kenta kawachi <kentakawachi@kentanoMacBook-Air.local>
2025-06-09 18:01:27 +0900

    新しいシーンの設定といざ出陣ボタン

---
+++ Assets/Script/IzaBotan.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class IzaBotan : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void OnBattleStartButtonPressed()
+    {
+        Debug.Log("戦闘開始！");
+        // 必要ならシーン遷移などの処理をここに追加
+    }
+}
